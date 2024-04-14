80603317cc53c1ae581e7b786a3274ec0fa1b703
Vyacheslav Kalin <provak.gg@gmail.com>
2024-04-14 09:39:51 +0600

    Changed method Index for adding pagination; Added Testing for pagination

--- SportsStore.Tests/HomeControllerTests.cs
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -31,5 +31,32 @@ namespace SportsStore.Tests
             Assert.Equal("P1", prodArray[0].Name);
             Assert.Equal("P2", prodArray[1].Name);
         }
+
+        [Fact]
+        public void CanPaginate()
+        {
+            // Организация 
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] 
+            {
+                new Product { ProductId = 1, Name = "P1" },
+                new Product { ProductId = 2, Name = "P2" },
+                new Product { ProductId = 3, Name = "P3" },
+                new Product { ProductId = 4, Name = "P4" },
+                new Product { ProductId = 5, Name = "P5" }
+            }).AsQueryable);
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+
+            // Действие
+            IEnumerable<Product> result = (controller.Index(2) as ViewResult).ViewData.Model 
+                as IEnumerable<Product>;
+
+            // Утверждение
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
     }
 }
--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 
@@ -6,12 +7,17 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private readonly IStoreRepository _repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repository)
         {
             _repository = repository;
         }
 
-        public IActionResult Index() => View(_repository.Products);
+        public IActionResult Index(int productPage = 1) 
+            => View(_repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
     }
 }
