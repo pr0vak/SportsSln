bcf24c5338df13af5e49a467cafe21228d07c488
Vyacheslav Kalin <provak.gg@gmail.com>
2024-05-23 11:56:54 +0600

    Added directory management

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -6,11 +6,28 @@ namespace SportsStore.Models
     {
         private StoreDbContext context;
 
-        public IQueryable<Product> Products => context.Products;
-
         public EFStoreRepository(StoreDbContext ctx)
         {
             context = ctx;
-        }   
+        }
+
+        public IQueryable<Product> Products => context.Products;
+
+        public void CreateProduct(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(Product p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+
+        public void SaveProduct(Product p)
+        {
+            context.SaveChanges();
+        }
     }
 }
--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsStore.Models
@@ -6,13 +7,17 @@ namespace SportsStore.Models
     {
         public long ProductId { get; set; }
 
+        [Required(ErrorMessage = "Пожалуйста, введите наименование товара")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Пожалуйста, введите описание")]
         public string Description { get; set; }
 
         [Column(TypeName = "decimal(8, 2)")]
+        [Required(ErrorMessage = "Пожалуйста, введите положительную цену")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Пожалуйста, укажите категорию")]
         public string Category { get; set; }
     }
 }
