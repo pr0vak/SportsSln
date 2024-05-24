1005d2aa209d4ac327ab38362b430b967280ddaf
Vyacheslav <slava93omsk@mail.ru>
2024-05-24 11:53:50 +0600

    Added package Microsoft.AspNetCore.Identity.EntityFrameworkCore; Added AppIdentityDbContex; Added IdentitySeedData for configuring DB;

---
+++ SportsStore/Models/IdentitySeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Secret123$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            AppIdentityDbContext context = app.ApplicationServices
+                .CreateScope().ServiceProvider
+                .GetRequiredService<AppIdentityDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            UserManager<IdentityUser> userManager = app.ApplicationServices
+                .CreateScope().ServiceProvider
+                .GetRequiredService<UserManager<IdentityUser>>();
+            IdentityUser user = await userManager.FindByIdAsync(adminUser);
+
+            if (user == null)
+            {
+                user = new IdentityUser(adminUser);
+                user.Email = "admin@example.com";
+                user.PhoneNumber = "555-1234";
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,6 +32,12 @@ namespace SportsStore
             services.AddScoped(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddServerSideBlazor();
+            services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:IdentityConnection"]);
+            });
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -40,6 +47,10 @@ namespace SportsStore
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints => 
             {
                 endpoints.MapControllerRoute("catpage", "{category}/Page{productPage:int}",
@@ -57,6 +68,7 @@ namespace SportsStore
                 endpoints.MapFallbackToPage("admin/{*catchall}", "/Admin/Index");
             });
             SeedData.EnsurePopulated(app);
+            IdentitySeedData.EnsurePopulated(app);
         }
     }
 }
